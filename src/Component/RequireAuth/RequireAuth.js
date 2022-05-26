087eeafe15c27a190aa7465f19f18a0deab65868
mynul <homeexercise8@gmail.com>
2022-05-26 23:06:52 +0600

    require auth

--- src/Component/RequireAuth/RequireAuth.js
+++ src/Component/RequireAuth/RequireAuth.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../firebase.init';
-
+// it will require for protected route
 const RequireAuth = ({children}) => {
     const [user, loading, error] = useAuthState(auth);
     let location=useLocation()
